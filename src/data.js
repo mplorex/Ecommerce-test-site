6b365c0664d62748adae5f3296916a26718a2c56
michael posselt <michaelposselt@outlook.com>
2021-06-09 12:32:45 -0500

    ecommerce test site

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export default {
+    products: [
+        {
+        _id: "be9c8541c9d440000665243",
+        name: "Norbert",
+        price: 2900,
+        image: "teddy_1.jpg",
+        description: "This is teddy #1."
+        },
+        {    
+        _id: "5beaa8bf1c9d440000a57d94",
+        name: "Arnold",
+        price: 3900,
+        image: "teddy_2.jpg",
+        description: "This is teddy #2"
+        },
+        {
+        _id: "5beaaa8f1c9d440000a57d95",
+        name: "Lenn and Carl",
+        price: 5900,
+        description: "This is teddy #3",
+        image: "teddy_3.jpg"
+        },
+        {
+        _id: "5beaabe91c9d440000a57d96",
+        name: "Gustav",
+        price: 4500,
+        image: "teddy_4.jpg",
+        description: "This is teddy #4"
+        },
+        {
+        _id: "5beaacd41c9d440000a57d97",
+        name: "Garfunkel",
+        price: 5500,
+        description: "This is teddy #5",
+        image: "teddy_5.jpg"
+    }
+    ],
+};
